b6a9bbdc5f93af209b6317ef89277f6892bf93c0
charlesnathansmith <charlesnathansmith@gmail.com>
2023-05-07 07:14:34 -0400

    Add files via upload

--- comp_sub1/esethash.cpp
+++ comp_sub1/esethash.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <string>
 #include "sha1.h"
+#include "sha512.h"
 
-const wchar_t pw[] = L"ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+const wchar_t pw[] = L"123456";
 
 const unsigned char hard_bytes[] = "\xE4\x62\x2C\xDB\x5F\xA8\x45\x1E\xA9\xBE\x3D\xB6\xC3\x2F\x06\xA5"
                                    "\xDD\x51\xB1\x9E\x1D\x47\x4A\x12\x5C\xDC\x7B\xAB\xB4\x07\xCB\xC4";
@@ -29,11 +31,21 @@ int main()
     // Buffers repeatedly used during hashing loop
     uint8_t pw_digest[3][64];
 
-    // SHA-1 digest of widechar-format password, padded to 64 bytes with 00
-    SHA1Init(&sha);
-    SHA1Update(&sha, (uint8_t*)pw, wcslen(pw) * 2);
-    SHA1Final(pw_digest[0], &sha);
-    memset(pw_digest[0] + 20, 0, 64 - 20);
+    // SHA-1 digest of widechar-format password if necessary to shrink it,
+    // otherwise use as is. Padded to 64 bytes with 00
+    size_t pw_len = wcslen(pw);
+    memset(pw_digest, 0, 64);
+
+    if (pw_len > 32)
+    {
+            SHA1Init(&sha);
+            SHA1Update(&sha, (uint8_t*)pw, pw_len * 2);
+            SHA1Final(pw_digest[0], &sha);
+    }
+    else
+    {
+        memcpy(pw_digest, pw, pw_len * 2);
+    }
 
     // XOR-encoded versions of password digest
     for (size_t i = 0; i < 64; i++)
@@ -43,7 +55,6 @@ int main()
         pw_digest[2][i] = c ^ 0x6A;
     }
 
-    /*
     puts("Password digest");
     print_buffer(pw_digest[0], 64);
 
@@ -52,7 +63,6 @@ int main()
 
     puts("Password digest ^ 0x36 ^ 0x6A");
     print_buffer(pw_digest[2], 64);
-    */
 
     // Intermediate digests
     uint8_t intermed[2][64];
@@ -115,4 +125,19 @@ int main()
 
     printf("combined hash:\t");
     print_buffer(fin_hash, 32);
+
+    // SHA-512 hashing
+    uint8_t sha512_digest[64];
+    SHA512Digest(sha512_digest, fin_hash, 32);
+
+    puts("\nSHA512");
+    print_buffer(sha512_digest, 64);
+    
+    // Prepare hash buffer for next stage
+    // This splits it up into 48-bit numbers, with the most significant bits
+    // of the least significant bytes constrained on the first two
+    //uint8_t mod_sha512_digest[40];
+    //*((uint64_t*) &mod_sha512_digest[0]) = *((uint64_t*) sha512_digest[0]) & 0xffffffffffff
+
+    return 0;
 }
